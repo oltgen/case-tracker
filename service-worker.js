730aa1fa28ef12e54133d79f3941a699be9cced2
oltgen <erickolteanu@gmail.com>
2026-03-16 17:21:29 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "case-tracker-v1";
+const URLS_TO_CACHE = [
+  "/case-tracker/",
+  "/case-tracker/index.html",
+  "/case-tracker/manifest.json",
+  "/case-tracker/service-worker.js",
+  "/case-tracker/icons/icon-192.png",
+  "/case-tracker/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          if (
+            !networkResponse ||
+            networkResponse.status !== 200 ||
+            networkResponse.type !== "basic"
+          ) {
+            return networkResponse;
+          }
+
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => caches.match("/case-tracker/index.html"));
+    })
+  );
+});
